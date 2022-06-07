970b33de6595d2f0ec9d625e2c4c5dafdacacf25
0x107a <0x107a@gmail.com>
2022-06-07 10:59:53 -0700

    ocaml

--- ocaml/cps.ml
+++
@@ -1,7 +0,0 @@
-(*
-  example of continuation passing style to achieve a tail recursive equivalent to a recursive function 
-  CPS is also utilized within compiler optimizations, as some compilers will automatically optimize
-  for this.
-
-  
-*)
